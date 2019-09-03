8680a203a561a0da5b4c52f30b5e161db333327d
Khusharth <khusharth19@gmail.com>
2019-09-03 17:58:06 +0530

    Added comments

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-// var li = document.querySelector("li");
 
+// To check length of input
 function inputLength() {
     return input.value.length;
 }
 
+// To Create a new li and button element
 function createListElement() {
     var li = document.createElement("li");
     var button = document.createElement("button");
@@ -17,15 +18,31 @@ function createListElement() {
     input.value = "";
 }
 
+// Check for input greater than 0 and then add li on click 
 function addListAfterClick() {
     if (inputLength() > 0) {
         createListElement();
         }
 }
 
+// To add li after Enter key is pressed
 function addListAfterKeypress(event) {
     if (inputLength() > 0 && event.keyCode === 13) {
         createListElement();
+    }
+}
+
+// To toogle between done and not done class
+function toggleDone() {
+    if (event.target.matches("li")) {
+        event.target.classList.toggle("done");
+    }
+}
+
+// To add Remove Class
+function removeElement() {
+    if (event.target.matches("button")) { 
+        event.target.parentNode.classList.add("remove");
     }
 }
 
@@ -33,13 +50,6 @@ button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
 
-ul.addEventListener("click",function(e) {
-   if (event.target.matches("li")) 
-    event.target.classList.toggle("done");
-  });
+ul.addEventListener("click", toggleDone);
+ul.addEventListener("click", removeElement);
 
-ul.addEventListener("click",function(e) {
-    if (event.target.matches("button")) 
-        event.target.parentNode.classList.toggle("remove");
-    });
-  
